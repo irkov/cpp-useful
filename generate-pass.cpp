fdea5259f50b102fd1504ac4549aa49f93982f13
irkov <irkov.alexey@gmail.com>
2022-04-24 23:21:56 +0700

    Upd: advanced_random() function added

--- generate-pass.cpp
+++ generate-pass.cpp
@@ -3,24 +3,51 @@
 #include <time.h>
 using namespace std;
 
-//main function
-int main()
-{
-  srand(time(0)); //seeding for rand()
-  string pass;
+//all data
+string data = "abcdefghijklmnopqrstuvwxyz"
+              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+              "0123456789"
+              "!@#$%^&*()";
 
-  //all data
-  string data = "abcdefghijklmnopqrstuvwxyz"
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                "0123456789"
-                "!@#$%^&*()";
+string sample_random()
+{
+  string pass;
 
   for (int i = 1; i <= 12; i++)
   {
     pass = pass + data[rand() % 72]; //getting generated char
   }
 
-  cout<<"Your password: "<<pass<<endl;
+  return pass;
+}
+
+
+string advanced_random()
+{
+  string pass;
+
+  for (int i = 1; i<= 3; i++)
+  {
+    for (int i = 1; i <= 6; i++)
+    {
+      pass = pass + data[rand() % 72]; //getting generated char
+    }
+    if (i < 3)
+    {
+      pass = pass + "-";
+    }
+  }
+
+
+  return pass;
+}
+
+//main function
+int main()
+{
+  srand(time(0)); //seeding for rand()
+  string result = advanced_random();
+  cout<<"Your password: "<<result<<endl;
 
   return 0;
 }
